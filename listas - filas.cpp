9293e0704a8bb2fe912282215ab00b647c0f6b38
isabelafdevito <80789978+isabelafdevito@users.noreply.github.com>
2022-09-08 22:26:11 -0300

    Create listas - filas.cpp

---
+++ listas - filas.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<list>
+using namespace std;
+
+int main () {
+	list<int> fila; // ponteiro para pilha;
+	int x; 
+	int i;
+	
+	// ler dados e inserir na pilha 
+	for(i=0;i<4;i++) {
+		cin >>x;
+		fila.push_back(x);
+		// na pilha, cada novo numero é colocado na frente do anterior
+		//na lista, cada novo numero é colocado depois do anterior
+	}
+	
+	// remover os dados da pilha e ir mostrando
+	while(!fila.empty()) {
+	x=*fila.begin();
+	cout << x << " ";
+	fila.pop_front();
+	}
+	return 0;
+}
