687f96015bffeff2e1997345693650c8ad938109
isabelafdevito <80789978+isabelafdevito@users.noreply.github.com>
2022-09-08 23:52:59 -0300

    Update arvores - quantidade de folhas.cpp

--- arvores - quantidade de folhas.cpp
+++ arvores - quantidade de folhas.cpp
@@ -6,9 +6,9 @@ using namespace std;
 
 struct treenode
 {
-	int info;
-	treenode *esq;
-	treenode *dir;
+    int info;
+    treenode *esq;
+    treenode *dir;
 
 };
 
@@ -18,124 +18,70 @@ typedef treenode *treenodeptr;
 
 void tInsere(treenodeptr &p, int x)
 {
-	if (p == NULL) // insere na raiz
-	{
-		p = new treenode;
-		p->info = x;
-		p->esq = NULL;
-		p->dir = NULL;
-	}
-	else if (x < p->info) // insere na subarvore esquerda
-		tInsere(p->esq, x);
-	else // insere na subarvore direita
-		tInsere(p->dir, x);
+    if (p == NULL) // insere na raiz
+    {
+        p = new treenode;
+        p->info = x;
+        p->esq = NULL;
+        p->dir = NULL;
+    }
+    else if (x < p->info) // insere na subarvore esquerda
+        tInsere(p->esq, x);
+    else // insere na subarvore direita
+        tInsere(p->dir, x);
 }
 
-treenodeptr tPesq(treenodeptr p, int x)
-{
-	if (p == NULL) // elemento n~ao encontrado
-		return NULL;
-	else if (x == p->info) // elemento encontrado na raiz
-		return p;
-	else if (x < p->info) // procura na sub�arvore esquerda
-		return tPesq(p->esq, x);
-	else // procura na sub�arvore direita
-		return tPesq(p->dir, x);
 
-
-}
-
-void emNivel(treenodeptr t)
-{
-	treenodeptr n;
-	list<treenodeptr> q;
-	if (t != NULL)
-	{
-		q.push_back(t);
-		while (!q.empty())
-		{
-			n = *q.begin();
-			q.pop_front();
-			if (n->esq != NULL)
-				q.push_back(n->esq);
-			if (n->dir != NULL)
-				q.push_back(n->dir);
-			cout << n->info << " ";
-		}
-		cout << endl;
-	}
-}
-
-void posOrdem (treenodeptr arvore)
-{
-	if (arvore != NULL)
-	{
-
-		posOrdem(arvore->esq);
-		posOrdem(arvore->dir);
-
-		cout << arvore->info << " ";
-	}
-}
 
 void tDestruir (treenodeptr &arvore)
 {
-	if (arvore != NULL)
-	{
-		tDestruir(arvore->esq);
-		tDestruir(arvore->dir);
-		delete arvore;
-	}
-	arvore = NULL;
+    if (arvore != NULL)
+    {
+        tDestruir(arvore->esq);
+        tDestruir(arvore->dir);
+        delete arvore;
+    }
+    arvore = NULL;
 }
 
 int  Folhas(treenodeptr arvore)
 {
-	int f = 0;
-	if (arvore != NULL)
-	{
-		if(arvore->dir == 0 && arvore->esq == 0)
-			f++;
-		
-	f +=	Folhas(arvore->dir);
-	//	cout << arvore->info << " ";
-	f +=	Folhas(arvore->esq);
-	}
-	return f;
+    int f = 0;
+    if (arvore != NULL)
+    {
+        if(arvore->dir == 0 && arvore->esq == 0)
+            f++;
+
+        f +=	Folhas(arvore->dir);
+        //	cout << arvore->info << " ";
+        f +=	Folhas(arvore->esq);
+    }
+    return f;
 }
 
-void preOrdem (treenodeptr arvore)
-{
-	if (arvore != NULL)
-	{
-	cout  << arvore->info << " ";
-		preOrdem(arvore->esq);
-		preOrdem(arvore->dir);
-	}
-}
 
 
 
 int main()
 {
-	int x = 0;
-	int f;
-	treenodeptr arvore = NULL;
-	treenodeptr p = NULL;
+    int x = 0;
+    int f;
+    treenodeptr arvore = NULL;
+    treenodeptr p = NULL;
 
 
-	cin >> x;
+    cin >> x;
 
-	while(x != -1)
-	{
-		tInsere(arvore, x);
-		cin >> x;
+    while(x != -1)
+    {
+        tInsere(arvore, x);
+        cin >> x;
 
-	}
+    }
 
-	  f = Folhas(arvore);
-	  cout << f << endl;
-		tDestruir(arvore);
+    f = Folhas(arvore);
+    cout << f << endl;
+    tDestruir(arvore);
 
-	return 0;
+    return 0;
 }
