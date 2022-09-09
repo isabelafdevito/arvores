a503425abc22168bc956e4139d00f351b67d7b1e
isabelafdevito <80789978+isabelafdevito@users.noreply.github.com>
2022-09-09 00:20:31 -0300

    Update busca sequencial.cpp

--- busca sequencial.cpp
+++ busca sequencial.cpp
@@ -1,22 +1,22 @@
 #include<iostream>
 using namespace std;
- 
+
 int sequencial(int vet[], int tam, int x) {
-    bool achou = false; 
-    int i=0; 
-    while (achou==false && i < tam) 
-        achou=vet[i++]==x; 
-    if (achou) 
-    	cout << x << " encontrado na posicao " << i-1 << endl;
-    else 
+    bool achou = false;
+    int i=0;
+    while (achou==false && i < tam)
+        achou=vet[i++]==x;
+    if (achou)
+        cout << x << " encontrado na posicao " << i-1 << endl;
+    else
         cout << x << " nao encontrado" << endl;
     return 0;
 }
 
 int main () {
     int vetor[100];
-    int tamanho; 
-    int y; 
+    int tamanho;
+    int y;
     int a=0;
     cin >> vetor[a];
     while (vetor[a]!=-1) {
