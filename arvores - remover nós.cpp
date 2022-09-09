825ecf7841048b0bd875fde1ca52761cc710033b
isabelafdevito <80789978+isabelafdevito@users.noreply.github.com>
2022-09-08 23:57:37 -0300

    Update arvores - remover nós.cpp

--- arvores - remover nós.cpp
+++ arvores - remover nós.cpp
@@ -6,9 +6,9 @@ using namespace std;
 
 struct treenode
 {
-	int info;
-	treenode *esq;
-	treenode *dir;
+    int info;
+    treenode *esq;
+    treenode *dir;
 
 };
 
@@ -20,88 +20,88 @@ int y;
 
 void tInsere(treenodeptr &p, int x)
 {
-	if (p == NULL) // insere na raiz
-	{
-		p = new treenode;
-		p->info = x;
-		p->esq = NULL;
-		p->dir = NULL;
-	}
-	else if (x < p->info) // insere na subarvore esquerda
-		tInsere(p->esq, x);
-	else // insere na subarvore direita
-		tInsere(p->dir, x);
+    if (p == NULL) // insere na raiz
+    {
+        p = new treenode;
+        p->info = x;
+        p->esq = NULL;
+        p->dir = NULL;
+    }
+    else if (x < p->info) // insere na subarvore esquerda
+        tInsere(p->esq, x);
+    else // insere na subarvore direita
+        tInsere(p->dir, x);
 }
 
 
 treenodeptr tMenor(treenodeptr &raiz)
 {
-	treenodeptr t;
-	t = raiz;
-	if (t->esq == NULL) // encontrou o menor valor
-	{
-		raiz = raiz->dir;
-		return t;
-	}
-	else // continua a busca na sub�arvore esquerda
-		return tMenor(raiz->esq);
+    treenodeptr t;
+    t = raiz;
+    if (t->esq == NULL) // encontrou o menor valor
+    {
+        raiz = raiz->dir;
+        return t;
+    }
+    else // continua a busca na sub�arvore esquerda
+        return tMenor(raiz->esq);
 }
 
 int tRemove(treenodeptr &raiz, int x)
 {
-	treenodeptr p;
-	if (raiz == NULL) // �arvore vazia
-		return 1;
-	if (x == raiz->info)
-	{
-		p = raiz;
-		if (raiz->esq == NULL) // a raiz n~ao tem filho esquerdo
-			raiz = raiz->dir;
-		else if (raiz->dir == NULL) // a raiz n~ao tem filho direito
-			raiz = raiz->esq;
-		else // a raiz tem ambos os filhos
-		{
-			p = tMenor(raiz->dir);
-			raiz->info = p->info;
-		}
-		delete p;
-		return 0;
-	}
-	else if (x < raiz->info)
-		return tRemove(raiz->esq, x);
-	else
-		return tRemove(raiz->dir, x);
+    treenodeptr p;
+    if (raiz == NULL) // �arvore vazia
+        return 1;
+    if (x == raiz->info)
+    {
+        p = raiz;
+        if (raiz->esq == NULL) // a raiz n~ao tem filho esquerdo
+            raiz = raiz->dir;
+        else if (raiz->dir == NULL) // a raiz n~ao tem filho direito
+            raiz = raiz->esq;
+        else // a raiz tem ambos os filhos
+        {
+            p = tMenor(raiz->dir);
+            raiz->info = p->info;
+        }
+        delete p;
+        return 0;
+    }
+    else if (x < raiz->info)
+        return tRemove(raiz->esq, x);
+    else
+        return tRemove(raiz->dir, x);
 }
 
 void emOrdem (treenodeptr arvore)
 {
-	if (arvore != NULL)
-	{
-		emOrdem(arvore->esq);
-		cout << arvore->info << " ";
-	
-		emOrdem(arvore->dir);
-	}
+    if (arvore != NULL)
+    {
+        emOrdem(arvore->esq);
+        cout << arvore->info << " ";
+
+        emOrdem(arvore->dir);
+    }
 }
 
 
 int main()
 {
-	int x = 0;
-	treenodeptr arvore = NULL;
-	cin >> x;
+    int x = 0;
+    treenodeptr arvore = NULL;
+    cin >> x;
 
-	while(x != 0)
-	{
-		tInsere(arvore, x);
-		cin >> x;
-	}
-	cin >> y;
-	
-	while(tRemove(arvore, y) == 0){}
-		
-	emOrdem(arvore);
+    while(x != 0)
+    {
+        tInsere(arvore, x);
+        cin >> x;
+    }
+    cin >> y;
+
+    while(tRemove(arvore, y) == 0){}
+
+    emOrdem(arvore);
 
 
-	return 0;
+    return 0;
 }
